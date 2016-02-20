a81d05b97bf6276435b9801af1e126b262971b45
Davidthefat <davidthefat@gmail.com>
2016-02-20 14:14:54 -0700

    Implemented solution for problem 3.

---
+++ 3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned long primes[1000];
+	unsigned long ind = 1;
+	primes[0] = 1;
+	primes[1] = 2;
+	bool prime = false;
+	for(int i = 1; ind < 1000; i ++)
+	{
+		for(int t = 1; t < ind && prime; t++)
+			if(!(i % primes[t]))
+				prime = false;
+		if(prime)
+				primes[++ind] = i;
+		prime = true;
+	}
+	ind = 1;
+
+	unsigned long result = 600851475143;
+	unsigned long start = result;
+	while(ind < 1000)
+	{
+		if(primes[ind] > result)
+			break;
+		else if(!(result % primes[ind]))
+			result /= primes[ind];
+		else
+			ind ++;
+	}
+	cout << primes[ind] << endl;
+	
+	return 0;
+}
