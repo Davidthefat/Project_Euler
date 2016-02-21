5781e8e2d81682303dd3cd6222a395db0f6c6626
David Yoon <davidthefat@gmail.com>
2016-02-21 14:28:28 -0700

    Finished Problem 4.

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
+bool isPal(int in)
+{
+    int ind = 1000000;
+    int tail = 10;
+    bool result = 0;
+    while(!((in%ind)/(ind/10)))
+        ind /= 10;
+    while(((in%ind)/(ind/10)) == ((in%tail)/(tail/10)) && ind >= tail)
+    {
+        ind /= 10;
+        tail *= 10;
+    }
+    if(ind == tail || ind == (tail / 10))
+        result = 1;
+    return result;
+}
 int main()
 {
-    
+    int max = 0;
+    for(int i = 100; i < 1000; i++)
+        for(int j = 100; j < 1000; j++)
+            max = isPal(i*j) && i*j > max?i*j:max;
+    cout << max << endl;
     return 0;
 }
